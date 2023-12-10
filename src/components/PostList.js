3f99f27e081f154680944d6ac473cdad401e60ab
ameerrah9 <mccullers.ghameerah@gmail.com>
2023-12-09 21:59:03 -0500

    Simple React Admin Dashboard

---
+++ src/components/PostList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { List, Datagrid, TextField, EmailField } from 'react-admin';
+
+const PostList = () => {
+  return <div>PostList</div>;
+};
+
+export default PostList;
